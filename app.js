9325c87e5b183199ac938c344fed32f24ee4acb7
Krish Gupta <krishguptaa13@gmail.com>
2024-06-19 01:14:51 +0530

    asdf

--- app.js
+++ app.js
@@ -8,13 +8,14 @@ import 'dotenv/config'
 
 const app = express();
 app.use(cookieParser());
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+// app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({credentials:true, origin:process.env.BASE_URL}));
 app.use(express.json());
 app.use('/api', router);
 const PORT = process.env.PORT || 5000
 
 // connecting to mongodb
-mongoose.connect(process.env.MONGODB_URL).then(() => {
+mongoose.connect('mongodb+srv://krishgupta:3jplzAjKDutUCrJg@cluster0.eje9wku.mongodb.net/').then(() => {
     app.listen(PORT);
     console.log("Database connected! Listening to port 5000")
 }).catch((err) => {
