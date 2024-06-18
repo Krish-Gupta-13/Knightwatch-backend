a693bcd7d87fe46908fb1b4cfd82485b0e790e88
Krish Gupta <krishguptaa13@gmail.com>
2024-06-19 00:57:21 +0530

    changes

--- app.js
+++ app.js
@@ -11,11 +11,11 @@ app.use(cookieParser());
 app.use(cors({credentials:true, origin:'http://localhost:3000'}));
 app.use(express.json());
 app.use('/api', router);
-
+const PORT = process.env.PORT || 5000
 
 // connecting to mongodb
 mongoose.connect(process.env.MONGODB_URL).then(() => {
-    app.listen(5000);
+    app.listen(PORT);
     console.log("Database connected! Listening to port 5000")
 }).catch((err) => {
     console.log(err)
