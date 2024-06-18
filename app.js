6f2d310630be6cf50ce046f70d2916d20bfb089f
Krish Gupta <krishguptaa13@gmail.com>
2024-06-19 02:10:41 +0530

    adf

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ import 'dotenv/config'
 const PORT = process.env.PORT || 5000
 const app = express();
 app.use(cookieParser());
-app.use(cors({credentials:true, origin:'http://localhost:3000'}));
-// app.use(cors({credentials:true, origin:'https://knightwatch-backend-1.onrender.com'}));
+// app.use(cors({credentials:true, origin:'http://localhost:3000'}));
+app.use(cors({credentials:true, origin:'https://knightwatch.onrender.com'}));
 app.use(express.json());
 app.use('/api', router);
 
