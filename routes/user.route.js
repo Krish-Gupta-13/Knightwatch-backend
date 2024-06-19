9bfb0b6615561b988885341f8b08cabcc988e6cd
Krish Gupta <krishguptaa13@gmail.com>
2024-06-19 18:13:31 +0530

    logout changes

--- routes/user.route.js
+++ routes/user.route.js
@@ -4,7 +4,7 @@ import { Login } from "../controllers/user.controller.js";
 
 const router = express.Router();
 
-// routing to signup
+// routing to signup            
 router.post('/signup', Signup)
 router.post('/login', Login)
 router.get('/user', VerifyToken, GetUser)
