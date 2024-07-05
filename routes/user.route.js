fce3ff09b8e28f876b36683e68841010eb22411d
Krish Gupta <krishguptaa13@gmail.com>
2024-07-06 01:07:08 +0530

    starting profile backend part

--- routes/user.route.js
+++ routes/user.route.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { Signup, VerifyToken, GetUser, refreshToken, Logout } from "../controllers/user.controller.js";
 import { Login } from "../controllers/user.controller.js";
+// import { Edit } from "../controllers/profile.controller.js"
 
 const router = express.Router();
 
@@ -10,4 +11,5 @@ router.post('/login', Login)
 router.get('/user', VerifyToken, GetUser)
 router.get('/refresh', refreshToken, VerifyToken, GetUser);
 router.post('/logout', VerifyToken, Logout)
+// router.post('/edit', Edit)
 export default router
