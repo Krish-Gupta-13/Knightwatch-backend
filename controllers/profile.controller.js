01ed4799c99f99a0fe42cfd71834f65165431446
Krish Gupta <100077170+Krish-Gupta-13@users.noreply.github.com>
2024-07-09 18:40:25 +0530

    completed profile page

--- controllers/profile.controller.js
+++ controllers/profile.controller.js
@@ -1,26 +1,38 @@
 import { Profile } from '../models/profile.model.js'
 // import { User } from '../models/user.model.js'
 
+
+
+
+
 export const Edit = async (req, res, next) => {
-    const {location, college, github, linkedin, twitter, instagram, email, phone} = req.body;
-    const profile = new Profile({
-        college: college,
-        location: location,
-        github: github,
-        linkedin: linkedin,
-        twitter: twitter,
-        instagram: instagram,
-        email: email,
-        phone: phone
-    });
-    try{
-        await profile.save();
+    const { location, college, github, linkedin, twitter, instagram, email, phone } = req.body;
+    try {
+        const profile = await Profile.findOneAndUpdate(
+            { email: email }, 
+            {
+                college: college,
+                location: location,
+                github: github,
+                linkedin: linkedin,
+                twitter: twitter,
+                instagram: instagram,
+                phone: phone
+            },
+            { new: true, upsert: true } // Options: return the updated document and create if it doesn't exist
+        );
+
+        if (!profile) {
+            return res.status(404).json({ message: "Profile not found" });
+        }
+
+        return res.status(200).json({ message: "Profile updated successfully", profile });
+    } catch (err) {
+        console.error(err);
+        return res.status(500).json({ message: "An error occurred while updating the profile" });
     }
-    catch(err){
-        console.log(err)
-    }
-    return res.status(200).json({message:"update successfully"})
-}
+};
+
 
 export const GetProfileUser = async (req, res, next) => {
     const userEmail = req.email;
@@ -36,3 +48,26 @@ export const GetProfileUser = async (req, res, next) => {
     }
     return res.status(200).json({profileUser})
 }
+
+
+
+// export const Edit = async (req, res, next) => {
+//     const {location, college, github, linkedin, twitter, instagram, email, phone} = req.body;
+//     const profile = await Profile.updateOne({
+//         college: college,
+//         location: location,
+//         github: github,
+//         linkedin: linkedin,
+//         twitter: twitter,
+//         instagram: instagram,
+//         email: email,
+//         phone: phone
+//     }, { new: true, upsert: true });
+//     try{
+//         await profile.save();
+//     }
+//     catch(err){
+//         console.log(err)
+//     }
+//     return res.status(200).json({message:"update successfully"})
+// }
